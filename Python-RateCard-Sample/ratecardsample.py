f0e48646d178745e0d2111b38b20ead32d9e52f3
PaulS <snymanpaul@gmail.com>
2019-08-06 18:59:38 -0700

    Add inline implementation for Oauth2 token
    The sample by itself wasn't that useful as it required you to know how to create a token and then pass that to this program. Added an inline implementation for the Oauth2 token that is then used to call the ratecard API

--- Python-RateCard-Sample/ratecardsample.py
+++ Python-RateCard-Sample/ratecardsample.py
@@ -1,14 +1,37 @@
 # Using this package which is a HTTP library
 import requests
  
-# Parameters need for API
-subscription = '<subscriptionId>'
-token = '<bearerToken>'
-offer = '<offerId>'
+# Parameters needed for ratecard API
+subscription = 'your subscription ID'                 # replace with your Azure subscription ID
+#token = '<bearerToken>'                              # need to determine token at runtime, implementation below
+offer = 'MS-AZR-0003p'                                # PAYG offer
 currency = 'USD'
 locale = 'en-US'
 region = 'US'
 rateCardUrl = "https://management.azure.com:443/subscriptions/{subscriptionId}/providers/Microsoft.Commerce/RateCard?api-version=2016-08-31-preview&$filter=OfferDurableId eq '{offerId}' and Currency eq '{currencyId}' and Locale eq '{localeId}' and RegionInfo eq '{regionId}'".format(subscriptionId = subscription, offerId = offer, currencyId = currency, localeId = locale, regionId = region)
+
+# Parameters needed for Oauth2 API
+grant_type = 'client_credentials'                     # static
+client_id = 'your client ID'                          # replace with your client ID (App registration in AAD) 
+client_secret = 'your client secret'                  # replace with your client secret (from the same App registration)
+resource = 'https://management.azure.com/'            # static
+oauthUrl1 = 'https://login.microsoftonline.com/'      # static
+tenant_id = 'yourAADtenant.onmicrosoft.com'           # replace with your AD tenant ID
+oauthUr2 = '/oauth2/token'                            # static
+
+# Prepare paramters to be sent to api for Oauth2 token
+URL = oauthUrl1 + tenant_id + '/oauth2/token'
+
+PARAMS = {  'grant_type':    grant_type, 
+            'client_id':     client_id,
+            'client_secret': client_secret, 
+            'resource':      resource}
+
+# Request an OAuth2 bearer token, sending post request and saving response as response object 
+r1 = requests.post(url = URL, data = PARAMS)
+
+# Extract the Oauth2 token from the response
+token = r1.json()['access_token']
 
 # Don't allow redirects and call the RateCard API
 response = requests.get(rateCardUrl, allow_redirects=False, headers = {'Authorization': 'Bearer %s' %token})
